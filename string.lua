ad48bd9aaf8e0e85f6708b1ad072a69a22144e42
Cassielu123 <1227984391@qq.com>
2022-12-22 12:30:29 +0800

    新增内容

--- string.lua
+++ string.lua
@@ -3,15 +3,15 @@ local strings = {
     CHARACTERS = {
         WINONA = {
             DESCRIBE = {
-                ALOE = "吃下它会好吗？",  -- "Is it health benefits?"
+                ALOE = "吃下它会好吗？", 
                 ALOE_COOKED = "应该不会有问题，尝试一下吧。",
-                ALOE_PLANTED = "一株奇怪的植物。", -- "A weird plant."
-                ALOE_SEEDS = "一把种子。",  -- "A handful of seeds."
+                ALOE_PLANTED = "一株奇怪的植物。", 
+                ALOE_SEEDS = "一把种子。", -- 联机版种子台词
 
                 RADISH = "非常火辣的萝卜。",
                 RADISH_COOKED = "粗糙的加工适得其反。",
-                RADISH_PLANTED = "欢迎来拔。",  -- "Perfectly pluckable."
-                RADISH_SEEDS = "一把种子。",  -- "A handful of seeds."
+                RADISH_PLANTED = "欢迎来拔。",  -- 和胡萝卜一样的台词
+                RADISH_SEEDS = "一把种子。",  -- 联机版种子台词
 
                 RELIC_1 = "会有有钱人想要这个的。",
                 PIG_RUINS_IDOL = "一定有什么办法把上面那部分卸下来。",
@@ -27,7 +27,7 @@ local strings = {
 
                 PIG_RUINS_PIG = "未免太过于张扬了。",
 
-                BASEFAN = "老板一定会感兴趣的。",  -- "I'm sure bossman would be very interested in it."
+                BASEFAN = "老板一定会感兴趣的。",  -- 对应老瓦的basefan检查台词
 
                 PIG_SHOP_CITYHALL = {
                     GENERIC = "当官的工作的地方。",
@@ -44,6 +44,20 @@ local strings = {
                     SLEEPING = "睡吧，懒猪。",
                 },
                 APORKALYPSE_CLOCK = "一套相当复杂的仪表装置。",
+
+                WALL_PIG_RUINS = "一堵年久失修的墙。",
+
+                ANCIENT_HULK ="",
+                ANCIENT_ROBOTS_ASSEMBLY ="",
+                ANCIENT_ROBOT_CLAW ="",
+                ANCIENT_ROBOT_HEAD ="",
+                ANCIENT_ROBOT_LEG ="",
+                ANCIENT_ROBOT_RIBS ="",
+                LIVING_ARTIFACT ="",
+
+                OINC="这算是工资吗？",
+                OINC10="一点小积蓄，不要浪费。",
+                OINC100="勤奋是致富之本。",
             },
         },
         WORTOX = {
@@ -51,23 +65,23 @@ local strings = {
                 ALOE = "可以内服也可以外敷，我选择外敷。",
                 ALOE_COOKED = "对凡人们脆弱的身体有好处。",
                 ALOE_PLANTED = "是芦荟，凡人的食物。",
-                ALOE_SEEDS = "真是奇怪的小种子啊。",  -- "Strange little seeds, indeed, indeed."
+                ALOE_SEEDS = "真是奇怪的小种子啊。",  -- 联机版种子台词
 
                 RADISH = "有点辣。",
                 RADISH_COOKED = "辣死了辣死了。",
                 RADISH_PLANTED = "",
-                RADISH_SEEDS = "春种一粒粟，秋收万颗子！",  -- "Grow a seed and you shall feed!"
+                RADISH_SEEDS = "春种一粒粟，秋收万颗子！",  -- 联机版种子台词
 
                 RELIC_1 = "古代的小雕像，拿来装饰吧。",
                 PIG_RUINS_IDOL = "上面的小雕像不错啊，我们把它拿走吧。",
 
-                RELIC_2 = "我看看...“问号，流星，松树。”哈哈，开玩笑的。",  -- "Let me see...\"Question mark, shooting star, pine tree.\"Haha, just kidding"
-                PIG_RUINS_PLAQUE = "嘿！小树，猜猜这上面写的是什么？",  -- "Hey little tree,guess what this says?"
+                RELIC_2 = "我看看...“问号，流星，松树。”哈哈，开玩笑的。", --Gravity Falls
+                PIG_RUINS_PLAQUE = "嘿！小树，猜猜这上面写的是什么？", 
 
                 RELIC_3 = "大猪头的大鼻头。",
                 PIG_RUINS_HEAD = "别人有伞，它有大头！",
 
-                RELIC_4 = "看上去就非常皇家。",  -- "It looks very royal."
+                RELIC_4 = "看上去就非常皇家。",
                 RELIC_5 = "很多宝石，但没有魔法。",
 
                 PIG_RUINS_PIG = "笑口常开啊。",
@@ -85,55 +99,83 @@ local strings = {
                 PIG_PALACE = "花里胡哨的大房子。",
                 PIGMAN_QUEEN = "您好，陛下。",
                 PIGMAN_MAYOR = {
-                    GENERIC = "是，首相。",--Yes, Prime Minister.
+                    GENERIC = "是，首相。",--剧名
                     SLEEPING = "真是累死人，对吧。",
                 },
                 APORKALYPSE_CLOCK = "好大规模的魔法，天啊，这会很好玩的。",
+
+                WALL_PIG_RUINS = "这些符文不是能拦住我的那种。",
+
+                ANCIENT_HULK ="",
+                ANCIENT_ROBOTS_ASSEMBLY ="",
+                ANCIENT_ROBOT_CLAW ="",
+                ANCIENT_ROBOT_HEAD ="",
+                ANCIENT_ROBOT_LEG ="",
+                ANCIENT_ROBOT_RIBS ="",
+                LIVING_ARTIFACT ="",
+
+                OINC="普通的硬币。",
+                OINC10="依然是普通的货币。",
+                OINC100="有钱能使鬼推磨，但他们毕竟只是猪。",
             },
         },
         WURT = {
             DESCRIBE = {
                 ALOE = "又甜又黏。",
-                ALOE_COOKED = "但这尝起来不像药啊，薇克巴顿女士。", -- "But it don't taste like medicine. Ms. Wickerbottom"
-                ALOE_PLANTED = "格，没见过的小吃",  -- "Grrr, snack never seen."
-                ALOE_SEEDS = "能长出更多的小吃，浮浪噗！", -- "Make more snacks, florp!"
+                ALOE_COOKED = "但这尝起来不像药啊，薇克巴顿女士。", 
+                ALOE_PLANTED = "格，没见过的小吃",
+                ALOE_SEEDS = "能长出更多的小吃，浮浪噗！",
 
-                RADISH = "辛辣的根！", -- "Hot spicy root!"
-                RADISH_COOKED = "没有比原本更好。", -- "Not better than it was."
-                RADISH_PLANTED = "格，没见过的小吃",  -- "Grrr, snack never seen."
-                RADISH_SEEDS = "能长出更多的小吃，浮浪噗！", -- "Make more snacks, florp!"
+                RADISH = "辛辣的根！","
+                RADISH_COOKED = "没有比原本更好。",
+                RADISH_PLANTED = "格，没见过的小吃", 
+                RADISH_SEEDS = "能长出更多的小吃，浮浪噗！", 
 
-                RELIC_1 = "猪人给我钱换它们自己的东西，好！", -- "Pigfolk pay me for their own stuff. Good!"
-                PIG_RUINS_IDOL = "一个猪人像，而不是鱼人。",  -- "A pigfolk idol, not mermfolk."
+                RELIC_1 = "猪人给我钱换它们自己的东西，好！", 
+                PIG_RUINS_IDOL = "一个猪人像，而不是鱼人。", 
 
-                RELIC_2 = "猪人给我钱换它们自己的东西，好！", -- "Pigfolk pay me for their own stuff. Good!"
-                PIG_RUINS_PLAQUE = "这也是猪人造的吗？",  -- "Created by pigfolk too?"
+                RELIC_2 = "猪人给我钱换它们自己的东西，好！",
+                PIG_RUINS_PLAQUE = "这也是猪人造的吗？", 
 
                 RELIC_3 = "战利品！",
-                PIG_RUINS_HEAD = "巨大的猪人脸。",  -- "Giant pifrolk face!"
+                PIG_RUINS_HEAD = "巨大的猪人脸。", 
 
-                RELIC_4 = "一个不一样的猪人，为什么？",  -- "A different pigfolk, why?"
+                RELIC_4 = "一个不一样的猪人，为什么？",
                 RELIC_5 = "不能吃，但闪闪发光。",
 
-                PIG_RUINS_PIG = "看我把猪人的牙齿拿走！",  -- "I pull out the pigfolk's teeth!"
+                PIG_RUINS_PIG = "看我把猪人的牙齿拿走！", 
 
-                BASEFAN = "水都跑掉了！浮浪噗的！",  -- "Water ran away! florpt!"
+                BASEFAN = "水都跑掉了！浮浪噗的！",  
 
                 PIG_SHOP_CITYHALL = {
-                    GENERIC = "鱼人必须也有一个！", --Mermfolk
+                    GENERIC = "鱼人必须也有一个！", 
                     BURNING= "烧吧！",
                 },
                 PIG_SHOP_CITYHALL_PLAYER = {
                     GENERIC = "猪人现在要听鱼人的！",
                     BURNING= "一定是猪人干的！",
                 },
-                PIG_PALACE = "鱼人必须也有一个！！！",--Mermfolk
-                PIGMAN_QUEEN = "她才不算什么女王呢，浮浪特！！",--SHe no King, flort!!对应猪王
+                PIG_PALACE = "鱼人必须也有一个！！！",
+                PIGMAN_QUEEN = "她才不算什么女王呢，浮浪特！！",--对应猪王
                 PIGMAN_MAYOR = {
                     GENERIC = "格勒噗！猪人的小头目。",--Glurp! A little big Pigfolk.
                     SLEEPING = "不许睡觉你这蠢猪！",--nasty Pigfolk!
                 },
                 APORKALYPSE_CLOCK = "坏故事里的东西！",
+
+                WALL_PIG_RUINS = "一堵破墙。",
+
+                ANCIENT_HULK ="",
+                ANCIENT_ROBOTS_ASSEMBLY ="",
+                ANCIENT_ROBOT_CLAW ="",
+                ANCIENT_ROBOT_HEAD ="",
+                ANCIENT_ROBOT_LEG ="",
+                ANCIENT_ROBOT_RIBS ="",
+                LIVING_ARTIFACT ="",
+
+                OINC="这有什么用？",
+                OINC10="什么是“钱”？",
+                OINC100="换东西的东西！“钱”！",
             },
         },
         WALTER = {
@@ -141,12 +183,12 @@ local strings = {
                 ALOE = "芦荟的用途也包括食用。",
                 ALOE_COOKED = "这样应该会更美味。",
                 ALOE_PLANTED = "我认识这个！是芦荟！有很多好用途。",
-                ALOE_SEEDS = "沃比，你看有什么好地方可以种这个吗？",  -- "Woby, do you see a good spot to plant these?"
+                ALOE_SEEDS = "沃比，你看有什么好地方可以种这个吗？",  -- 联机版种子台词
 
                 RADISH = "这种萝卜有可能会变成老鼠吗？",
                 RADISH_COOKED = "对。这是做熟的水萝卜。",
                 RADISH_PLANTED = "现在我们有更多的水萝卜了。耶。",
-                RADISH_SEEDS = "沃比，你看有什么好地方可以种这个吗？",  -- "Woby, do you see a good spot to plant these?"
+                RADISH_SEEDS = "沃比，你看有什么好地方可以种这个吗？",  -- 联机版种子台词
 
                 RELIC_1 = "一尊小小的猪人像。",
                 PIG_RUINS_IDOL = "看看我们发现了什么，沃比！",
@@ -179,6 +221,20 @@ local strings = {
                     SLEEPING = "市长先生就在地上睡着了。",
                 },
                 APORKALYPSE_CLOCK = "哦不！是末日预言！",
+
+                WALL_PIG_RUINS = "古人...古猪们建造了它。",
+
+                ANCIENT_HULK ="战争机器人！流言是真的！",
+                ANCIENT_ROBOTS_ASSEMBLY ="",
+                ANCIENT_ROBOT_CLAW ="",
+                ANCIENT_ROBOT_HEAD ="",
+                ANCIENT_ROBOT_LEG ="",
+                ANCIENT_ROBOT_RIBS ="一只机器蚂蚁？",
+                LIVING_ARTIFACT ="",
+
+                OINC="我的零花钱。",
+                OINC10="我的一点小钱。",
+                OINC100="我的一笔巨款。",
             },
         },
         WANDA = {
@@ -186,12 +242,12 @@ local strings = {
                 ALOE = "这可是好东西。",
                 ALOE_COOKED = "趁热吃吧。",
                 ALOE_PLANTED = "我们摘走它吧，这可能是最后一个了。",
-                ALOE_SEEDS = "既然现在就能吃，为什么还要浪费时间种呢？", -- "Why waste time growing them when I can just eat them now?"
+                ALOE_SEEDS = "既然现在就能吃，为什么还要浪费时间种呢？", -- 联机版种子台词
 
-                RADISH = "这绝对是萝卜。",  -- "I Said. It must be a turnip."
+                RADISH = "这绝对是萝卜。",
                 RADISH_COOKED = "搞定了，希望它值得。",
-                RADISH_PLANTED = "这肯定是萝卜了，我不会认错。",  -- "It's a turnip. I can't be wrong,"
-                RADISH_SEEDS = "既然现在就能吃，为什么还要浪费时间种呢？", -- "Why waste time growing them when I can just eat them now?"
+                RADISH_PLANTED = "这肯定是萝卜了，我不会认错。",  
+                RADISH_SEEDS = "既然现在就能吃，为什么还要浪费时间种呢？", -- 联机版种子台词
 
                 RELIC_1 = "并不是谁都能跨越漫长的时间。",
                 PIG_RUINS_IDOL = "我还以为会更深...噢，那是另一个。",
@@ -218,12 +274,26 @@ local strings = {
                     BURNING = "啊，对，这会发生...",
                 },
                 PIG_PALACE = "真漂亮，我们进去吧。",
-                PIGMAN_QUEEN = "一旦你深入了解，就会发现她非常冷血。",-- 对应猪王He can be reasonable enough once you get to know him."
+                PIGMAN_QUEEN = "一旦你深入了解，就会发现她非常冷血。",-- 对应猪王
                 PIGMAN_MAYOR = {
                     GENERIC = "你会继续当选，还是那个谁来着。",
                     SLEEPING = "珍惜清闲吧。",
                 },
                 APORKALYPSE_CLOCK = "掌握他们的动向...非常聪明的想法。",
+
+                WALL_PIG_RUINS = "古代城墙的一部分。",
+
+                ANCIENT_HULK ="相信我！这是值得的！",
+                ANCIENT_ROBOTS_ASSEMBLY ="为什么我要这么做来着？",
+                ANCIENT_ROBOT_CLAW ="我记得是尾巴...但看着起来更像手啊？",
+                ANCIENT_ROBOT_HEAD ="",
+                ANCIENT_ROBOT_LEG ="",
+                ANCIENT_ROBOT_RIBS ="",
+                LIVING_ARTIFACT ="",
+
+                OINC="希望这能值得我付出的时间。",
+                OINC10="买点什么好呢？",
+                OINC100="这上面是松露吗？我之前从没仔细看过！",
             },
         },
     }
---
+++ string_en.lua
@@ -0,0 +1,269 @@
+
+local strings = {
+    CHARACTERS = {
+        WINONA = {
+            DESCRIBE = {
+                ALOE = "Is it health benefits?",
+                --ALOE_COOKED = "",
+                ALOE_PLANTED = "A weird plant.",
+                ALOE_SEEDS = "A handful of seeds.",
+
+                --RADISH = "非常火辣的萝卜。",
+                --RADISH_COOKED = "粗糙的加工适得其反。",
+                RADISH_PLANTED = "Perfectly pluckable.", 
+                RADISH_SEEDS = "A handful of seeds.",  
+                --[[
+                RELIC_1 = "会有有钱人想要这个的。",
+                PIG_RUINS_IDOL = "一定有什么办法把上面那部分卸下来。",
+
+                RELIC_2 = "我猜它会有一些文化价值。",
+                PIG_RUINS_PLAQUE = "一定有什么办法把上面那部分卸下来。",
+
+                RELIC_3 = "终究是文物，没准猪人会想要。",
+                PIG_RUINS_HEAD = "一定有什么办法把中间那部分卸下来。",
+
+                RELIC_4 = "这个看起来不太一样，也许她很特殊。",
+                RELIC_5 = "镶嵌了许多宝石，一定很值钱。",
+
+                PIG_RUINS_PIG = "未免太过于张扬了。",
+                ]]
+                BASEFAN = "I'm sure bossman would be very interested in it.", 
+                --[[
+                PIG_SHOP_CITYHALL = {
+                    GENERIC = "当官的工作的地方。",
+                    BURNING= "没做好消防工作啊。",
+                },
+                PIG_SHOP_CITYHALL_PLAYER = {
+                    GENERIC = "我的新车间。",
+                    BURNING = "嘿！是谁干出来的？",
+                },
+                PIG_PALACE = "呃，有必要这么夸张吗？",
+                PIGMAN_QUEEN = "养尊处优的家伙。",
+                PIGMAN_MAYOR = {
+                    GENERIC = "四体不勤的家伙。",
+                    SLEEPING = "睡吧，懒猪。",
+                },
+                APORKALYPSE_CLOCK = "一套相当复杂的仪表装置。",
+               ]]  
+            },
+        },
+        WORTOX = {
+            DESCRIBE = {
+                --ALOE = "可以内服也可以外敷，我选择外敷。",
+                --ALOE_COOKED = "对凡人们脆弱的身体有好处。",
+                --ALOE_PLANTED = "是芦荟，凡人的食物。",
+                ALOE_SEEDS = "Strange little seeds, indeed, indeed.", 
+
+                --RADISH = "有点辣。",
+                --RADISH_COOKED = "辣死了辣死了。",
+                --RADISH_PLANTED = "",
+                RADISH_SEEDS = "Grow a seed and you shall feed!", 
+
+                --RELIC_1 = "古代的小雕像，拿来装饰吧。",
+                --PIG_RUINS_IDOL = "上面的小雕像不错啊，我们把它拿走吧。",
+
+                RELIC_2 = "Let me see...\"Question mark, shooting star, pine tree.\"Haha, just kidding.",
+                PIG_RUINS_PLAQUE = "Hey little tree,guess what this says?",
+
+                --RELIC_3 = "大猪头的大鼻头。",
+                --PIG_RUINS_HEAD = "别人有伞，它有大头！",
+
+                RELIC_4 = "It looks very royal.",
+                --[[
+                RELIC_5 = "很多宝石，但没有魔法。",
+
+                PIG_RUINS_PIG = "笑口常开啊。",
+
+                BASEFAN = "微风拂面，好不快活。",
+
+                PIG_SHOP_CITYHALL = {
+                    GENERIC = "朱门酒肉臭。",
+                    BURNING= "付之一炬，可怜焦土！",
+                },
+                PIG_SHOP_CITYHALL_PLAYER ={
+                    GENERIC = "我是这里的市长了，哼！",
+                    BURNING= "付之一炬，可怜焦土！",
+                },
+                PIG_PALACE = "花里胡哨的大房子。",
+                PIGMAN_QUEEN = "您好，陛下。",
+                PIGMAN_MAYOR = {
+                    GENERIC = "Yes, Prime Minister.",
+                   SLEEPING = "真是累死人，对吧。",
+                },
+                APORKALYPSE_CLOCK = "好大规模的魔法，天啊，这会很好玩的。",
+
+                WALL_PIG_RUINS = "一堵年久失修的墙。",
+                ]]
+            },
+        },
+        WURT = {
+            DESCRIBE = {
+                --ALOE = "又甜又黏。",
+                ALOE_COOKED = "But it don't taste like medicine. Ms. Wickerbottom",
+                ALOE_PLANTED = "Grrr, snack never seen.", 
+                ALOE_SEEDS = "Make more snacks, florp!", 
+
+                RADISH = "Hot spicy root!", 
+                RADISH_COOKED = "Not better than it was.", 
+                RADISH_PLANTED = "Grrr, snack never seen.", 
+                RADISH_SEEDS = "Make more snacks, florp!", 
+
+                RELIC_1 = "Pigfolk pay me for their own stuff. Good!"", -- 
+                PIG_RUINS_IDOL = "A pigfolk idol, not mermfolk.", 
+
+                RELIC_2 = "Pigfolk pay me for their own stuff. Good!",
+                PIG_RUINS_PLAQUE = "Created by pigfolk too?", 
+
+                --RELIC_3 = "战利品！",
+                PIG_RUINS_HEAD = "Giant pifrolk face!", 
+
+                RELIC_4 = "A different pigfolk, why?",
+                --RELIC_5 = "不能吃，但闪闪发光。",
+
+                PIG_RUINS_PIG = "I pull out the pigfolk's teeth!",
+
+                BASEFAN = "Water ran away! florpt!",
+
+                PIG_SHOP_CITYHALL = {
+                    GENERIC = "Mermfolk must have one!",
+                    --BURNING= "烧吧！",
+                },
+                PIG_SHOP_CITYHALL_PLAYER = {
+                    GENERIC = "Pigfolk listen Mermfolk now!",
+                    BURNING= "It must be Pigfolk!",
+                },
+                PIG_PALACE = "Mermfolk must have one!!!",
+                PIGMAN_QUEEN = "She no Quenn, flort!!",
+                PIGMAN_MAYOR = {
+                    GENERIC = "Glurp! A little big Pigfolk.",
+                    SLEEPING = "No sleeping you nasty Pigfolk!",
+                },
+                APORKALYPSE_CLOCK = "Stuff form bad story!",
+
+                --WALL_PIG_RUINS = "",
+
+                --[[ANCIENT_HULK ="",
+                ANCIENT_ROBOTS_ASSEMBLY ="",
+                ANCIENT_ROBOT_CLAW ="",
+                ANCIENT_ROBOT_HEAD ="",
+                ANCIENT_ROBOT_LEG ="",
+                ANCIENT_ROBOT_RIBS ="",
+                LIVING_ARTIFACT ="",]]
+
+                OINC="What it for?",
+                OINC10="What is \"Gurrency\"?",
+                OINC100="Stuff for stuff! \"Gurrency\"!",
+            },
+        },
+        WALTER = {
+            DESCRIBE = {
+                --ALOE = "芦荟的用途也包括食用。",
+                --ALOE_COOKED = "这样应该会更美味。",
+                --ALOE_PLANTED = "我认识这个！是芦荟！有很多好用途。",
+                ALOE_SEEDS = "Woby, do you see a good spot to plant these?",
+
+                --RADISH = "这种萝卜有可能会变成老鼠吗？",
+                --RADISH_COOKED = "对。这是做熟的水萝卜。",
+                --RADISH_PLANTED = "现在我们有更多的水萝卜了。耶。",
+                RADISH_SEEDS = "Woby, do you see a good spot to plant these?",
+                --[[
+                RELIC_1 = "一尊小小的猪人像。",
+                PIG_RUINS_IDOL = "看看我们发现了什么，沃比！",
+
+                RELIC_2 = "你觉得它们会写什么呢？沃比？",
+                PIG_RUINS_PLAQUE = "是一个图腾！",
+
+                RELIC_3 = "一个巨大的鼻子。",
+                PIG_RUINS_HEAD = "沃比！它比你还要大！",
+
+                RELIC_4 = "非常有皇家风范。",
+                RELIC_5 = "看起来很宝贵。",
+
+                PIG_RUINS_PIG = "看它笑得多开心啊！",
+
+                BASEFAN = "啊，真舒服，沃比你也来吹吹。",
+
+                PIG_SHOP_CITYHALL = {
+                    GENERIC = "这一定是他们的政府部门！",
+                    BURNING= "消防！消防往这边来！",
+                },
+                PIG_SHOP_CITYHALL_PLAYER = {
+                    GENERIC = "这以后就是我们工作的地方了！沃比！",
+                    BURNING= "注意消防安全！我说什么来着！",
+                },
+                PIG_PALACE = "这一定是他们的皇宫！",
+                PIGMAN_QUEEN = "她穿的好华丽，但是闻起来...",
+                PIGMAN_MAYOR = {
+                    GENERIC = "请问您就是市长先生吗？",
+                    SLEEPING = "市长先生就在地上睡着了。",
+                },
+                APORKALYPSE_CLOCK = "哦不！是末日预言！",
+
+                WALL_PIG_RUINS = "古人...古猪们建造了它。",]]
+
+                ANCIENT_HULK ="War automation! It's real!",
+                --[[ANCIENT_ROBOTS_ASSEMBLY ="",
+                ANCIENT_ROBOT_CLAW ="",
+                ANCIENT_ROBOT_HEAD ="",
+                ANCIENT_ROBOT_LEG ="",]]
+                ANCIENT_ROBOT_RIBS ="A machine ant?",
+                --[[LIVING_ARTIFACT ="",
+
+                OINC="",
+                OINC10="",
+                OINC100="",
+                ]]
+            },
+        },
+        WANDA = {
+            DESCRIBE = {
+                --ALOE = "这可是好东西。",
+                --ALOE_COOKED = "趁热吃吧。",
+                --ALOE_PLANTED = "我们摘走它吧，这可能是最后一个了。",
+                ALOE_SEEDS = "Why waste time growing them when I can just eat them now?",
+
+                RADISH = "I Said. It must be a turnip.", 
+                --RADISH_COOKED = "搞定了，希望它值得。",
+                RADISH_PLANTED = "It's a turnip. I can't be wrong,", 
+                RADISH_SEEDS = "Why waste time growing them when I can just eat them now?",
+                --[[
+                RELIC_1 = "并不是谁都能跨越漫长的时间。",
+                PIG_RUINS_IDOL = "我还以为会更深...噢，那是另一个。",
+
+                RELIC_2 = "想必是古代猪人的手笔。",
+                PIG_RUINS_PLAQUE = "我很好奇它有什么含义，但还是算了吧。",
+
+                RELIC_3 = "大石脸的鼻子。",
+                PIG_RUINS_HEAD = "巨大的石头猪脸。",
+
+                RELIC_4 = "我们快去领赏吧。",
+                RELIC_5 = "就是它，我能用它换取丰厚的奖励。",
+
+                PIG_RUINS_PIG = "它在笑什么？",
+
+                BASEFAN = "非常有趣的结构，可惜还不够精细。",
+
+                PIG_SHOP_CITYHALL = {
+                    GENERIC = "没错，它在这条街上。",
+                    BURNING = "为什么会发生这种事情？",
+                },
+                PIG_SHOP_CITYHALL_PLAYER = {
+                    GENERIC = "我的办公室，不知道还能用多久。",
+                    BURNING = "啊，对，这会发生...",
+                },
+                PIG_PALACE = "真漂亮，我们进去吧。",
+                PIGMAN_QUEEN = "一旦你深入了解，就会发现她非常冷血。",
+                PIGMAN_MAYOR = {
+                    GENERIC = "你会继续当选，还是那个谁来着。",
+                    SLEEPING = "珍惜清闲吧。",
+                },
+                APORKALYPSE_CLOCK = "掌握他们的动向...非常聪明的想法。",
+
+                WALL_PIG_RUINS = "古代城墙的一部分。",
+                ]]
+            },
+        },
+    }
+}
+
+return strings
