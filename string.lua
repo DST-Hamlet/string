6a753d1ffb68fa2afa86d4c1050775b471dc827c
Cassielu123 <1227984391@qq.com>
2023-03-08 10:24:13 +0800

    new

--- string.lua
+++ string.lua
@@ -10,13 +10,13 @@ local strings = {
             ANNOUNCE_TOOLCORRODED = "这些工具的质量太差了。", --工具被腐蚀，未使用？
             ANNOUNCE_TURFTOOHARD = "这里的土层相当瓷实。", --挖不起来这个地皮
             ANNOUNCE_GAS_DAMAGE = "咳！瓦斯！咳！有瓦斯！", --毒气雨林伤害
-        
+            ]]
             ANNOUNCE_SNEEZE = "啊啾！",	--打喷嚏
             ANNOUNCE_HAYFEVER = "我的鼻子好痒...",	--开始有花粉
             ANNOUNCE_HAYFEVER_OFF = "呼，终于舒服了。",	--花粉消失
-        
+            
             ANNOUNCE_PICKPOOP = {"噫！","太脏了！","难以忍受！",},	--翻粪堆
-
+            --[[
             ANNOUNCE_TOO_HUMID = {"%s一点都不透气。","这%s闷得我要喘不过气来了。"},	--某衣服太热了
             ANNOUNCE_DEHUMID = {"",""},	--靠近风扇？总之是雾效果结束的
         
@@ -108,20 +108,20 @@ local strings = {
         },
         WORTOX = {
            --[[ 
-            ANNOUCE_UNDERLEAFCANOPY = "",
+            ANNOUCE_UNDERLEAFCANOPY = "生长的枝条遮蔽了阳光。",
             ANNOUCE_ALARMOVER = "",
-            ANNOUCE_BATS = "",--蝙蝠来咯
+            ANNOUCE_BATS = "哼哼！吸血鬼来们了！",--蝙蝠来咯
             ANNOUCE_OTHERWORLD_DEED = "",--其它世界的地契
             ANNOUNCE_TOOLCORRODED = "", --？？？
             ANNOUNCE_TURFTOOHARD = "", --挖不起来这个地皮
             ANNOUNCE_GAS_DAMAGE = "", --毒气雨林伤害
-        
-            ANNOUNCE_SNEEZE = "",	--打喷嚏
-            ANNOUNCE_HAYFEVER = "",	--开始有花粉
-            ANNOUNCE_HAYFEVER_OFF = "",	--花粉消失
-        
-            ANNOUNCE_PICKPOOP = {"","","",},	--翻粪堆
-
+            ]]
+            ANNOUNCE_SNEEZE = "（吸气）啊啾！啊啊啊啾！",	--sniff
+            ANNOUNCE_HAYFEVER = "哎呀，好多花粉，要难受了。",	--开始有花粉
+            ANNOUNCE_HAYFEVER_OFF = "花粉走光光了。终于。",	--花粉消失
+            
+            ANNOUNCE_PICKPOOP = {"真恶心！","这还是热的！","我的爪子不能要了！",},	--翻粪堆
+            --[[
             ANNOUNCE_TOO_HUMID = {"",""},	--某衣服太热了
             ANNOUNCE_DEHUMID = {"",""},	--靠近风扇？总之是雾效果结束的
         
@@ -203,20 +203,20 @@ local strings = {
         },
         WURT = {
             --[[
-            ANNOUCE_UNDERLEAFCANOPY = "",
-            ANNOUCE_ALARMOVER = "",
+            ANNOUCE_UNDERLEAFCANOPY = "大树挡住了光。",
+            ANNOUCE_ALARMOVER = "鱼人王国的故事还没完呢。",
             ANNOUCE_BATS = "",--蝙蝠来咯
             ANNOUCE_OTHERWORLD_DEED = "",--其它世界的地契
             ANNOUNCE_TOOLCORRODED = "", --？？？
             ANNOUNCE_TURFTOOHARD = "", --挖不起来这个地皮
             ANNOUNCE_GAS_DAMAGE = "", --毒气雨林伤害
-        
-            ANNOUNCE_SNEEZE = "",	--打喷嚏
-            ANNOUNCE_HAYFEVER = "",	--开始有花粉
-            ANNOUNCE_HAYFEVER_OFF = "",	--花粉消失
-        
-            ANNOUNCE_PICKPOOP = {"","","",},	--翻粪堆
-
+            ]]
+            ANNOUNCE_SNEEZE = "呵——呼，哈嚏呦！！！",	--打喷嚏
+            ANNOUNCE_HAYFEVER = "格勒。鳃好难受...",	--开始有花粉
+            ANNOUNCE_HAYFEVER_OFF = "噢！鳃终于舒服了！",	--花粉消失
+            
+            ANNOUNCE_PICKPOOP = {"好脏的水！","格勒...","没有下次了！",},	--翻粪堆
+            --[[
             ANNOUNCE_TOO_HUMID = {"",""},	--某衣服太热了
             ANNOUNCE_DEHUMID = {"",""},	--靠近风扇？总之是雾效果结束的
         
@@ -298,20 +298,20 @@ local strings = {
         },
         WALTER = {
             --[[
-            ANNOUCE_UNDERLEAFCANOPY = "",
+            ANNOUCE_UNDERLEAFCANOPY = "好茂盛的森林，一点光都进不来。",
             ANNOUCE_ALARMOVER = "",
             ANNOUCE_BATS = "",--蝙蝠来咯
             ANNOUCE_OTHERWORLD_DEED = "",--其它世界的地契
             ANNOUNCE_TOOLCORRODED = "", --？？？
             ANNOUNCE_TURFTOOHARD = "", --挖不起来这个地皮
             ANNOUNCE_GAS_DAMAGE = "", --毒气雨林伤害
-        
-            ANNOUNCE_SNEEZE = "",	--打喷嚏
-            ANNOUNCE_HAYFEVER = "",	--开始有花粉
-            ANNOUNCE_HAYFEVER_OFF = "",	--花粉消失
-        
-            ANNOUNCE_PICKPOOP = {"","","",},	--翻粪堆
+            ]]
+            ANNOUNCE_SNEEZE = "阿嚏！",	--打喷嚏
+            ANNOUNCE_HAYFEVER = "空气中有什么东西...我的鼻子好痒。",	--开始有花粉
+            ANNOUNCE_HAYFEVER_OFF = "我的鼻子好多了，它终于没了！",	--花粉消失
 
+            ANNOUNCE_PICKPOOP = {"松树先锋队员必须能吃苦耐劳...但...","这真的太不卫生了！","别过来！沃比！",},	--翻粪堆
+            --[[
             ANNOUNCE_TOO_HUMID = {"",""},	--某衣服太热了
             ANNOUNCE_DEHUMID = {"",""},	--靠近风扇？总之是雾效果结束的
         
@@ -394,19 +394,19 @@ local strings = {
         WANDA = {
             --[[
             ANNOUCE_UNDERLEAFCANOPY = "他们潜伏在这里！我需要光！",
-            ANNOUCE_ALARMOVER = "",
-            ANNOUCE_BATS = "",--蝙蝠来咯
-            ANNOUCE_OTHERWORLD_DEED = "",--其它世界的地契
+            ANNOUCE_ALARMOVER = "我们安全了，暂时。",
+            ANNOUCE_BATS = "为什么总有人找我们麻烦呢？",--蝙蝠来咯
+            ANNOUCE_OTHERWORLD_DEED = "这个地址实在太远了。",--其它世界的地契
             ANNOUNCE_TOOLCORRODED = "", --？？？
             ANNOUNCE_TURFTOOHARD = "", --挖不起来这个地皮
             ANNOUNCE_GAS_DAMAGE = "", --毒气雨林伤害
-        
-            ANNOUNCE_SNEEZE = "",	--打喷嚏
-            ANNOUNCE_HAYFEVER = "",	--开始有花粉
-            ANNOUNCE_HAYFEVER_OFF = "",	--花粉消失
-        
-            ANNOUNCE_PICKPOOP = {"为什么我要做这种事？","呕。",},	--翻粪堆
+            ]]
+            ANNOUNCE_SNEEZE = "阿嚏！",	--打喷嚏
+            ANNOUNCE_HAYFEVER = "这些植物除了到处撒粉没别的事干了吗？",	--开始有花粉
+            ANNOUNCE_HAYFEVER_OFF = "呼哈！终于结束了！",	--花粉消失
 
+            ANNOUNCE_PICKPOOP = {"为什么我要做这种事？","我一定是哪根筋搭错了。","呕。",},	--翻粪堆
+            --[[
             ANNOUNCE_TOO_HUMID = {"",""},	--某衣服太热了
             ANNOUNCE_DEHUMID = {"",""},	--靠近风扇？总之是雾效果结束的
         
@@ -486,17 +486,39 @@ local strings = {
                 OINC100="这上面是松露吗？我之前从没仔细看过！",
             },
         },
-        --[[
         WAGSTAFF = {
+            --[[
+            --戴各种眼镜的台词，可能会用得到。
+
+            --星象和沙漠护目镜，话说让老瓦自己的眼镜免疫风沙应该更好吧
+            ANNOUNCE_PUTONGOGGLES_MOONSTORM_GOGGLESHAT = "",
+            ANNOUNCE_PUTONGOGGLES_DESERTHAT = "",
+
+            --耕作先驱帽子
+            ANNOUNCE_PUTONGOGGLES_PLANTREGISTRYHAT = "",
+            ANNOUNCE_PUTONGOGGLES_NUTRIENTSGOGGLESHAT = "",
+
+            --骨头头盔
+            ANNOUNCE_PUTONGOGGLES_SKELETONHAT = "",
+            --启迪皇冠
+            ANNOUNCE_PUTONGOGGLES_ALTERGUARDIANHAT = "无限的洞察力！我甚至能看到世界的纤维在律动！",
+
+            --单机版那些，放在这里做参考。
+            ANNOUNCE_PUTONGOGGLES_GOGGLESARMORHAT = "现在我的颅骨已被保护起来以备战斗。", --"My cranium is now safeguarded for combat."
+            ANNOUNCE_PUTONGOGGLES_GOGGLESHEATHAT = "我透过红线眼镜看这个世界。", --"I see the world through infrared colored glasses."
+            ANNOUNCE_PUTONGOGGLES_GOGGLESNORMALHAT = "哈！当你能看清这个世界时，才能感受它更多有趣的地方。", --"Ah! The world is so much more interesting when you can see it!"
+            ANNOUNCE_PUTONGOGGLES_GOGGLESSHOOTHAT = "现在我可以发射破坏性的电磁辐射。", --"Now I can emit destructive spheres of electromagnetic radiation."
+            ]]
+
             DESCRIBE = {
                 --威尔逊
                 --我是火学家，我放火
                 LIGHTER =  "她随身携带的奇怪人造物。火是怎么产生的？", --An odd artifact she carries with her. How it work?
-                BERNIE_ACTIVE = "",
-                BERNIE_BIG = "",
+                BERNIE_ACTIVE = "它会与精神攻击产生反应！真迷人！", --"It responds to  psychic attacks! Fascinating!"
+                BERNIE_BIG = "不可思议。它表现得就像一个活物！",
                 BERNIE_INACTIVE = {
-                    GENERIC= "",
-                    BROKEN = "",
+                    GENERIC= "有些破旧的小熊玩偶，深受我合作伙伴们的喜爱。",
+                    BROKEN = "还能再修一下。",
                 },
 
                 --沃尔夫冈
@@ -512,18 +534,30 @@ local strings = {
                 --温蒂
                 SMALLGHOST = "不受黑暗燃料影响的年轻鬼魂。", --A younger poltergeist not affected by the dark fuel.
                 SISTURN = {
-                    GENERIC = "",
-                    SOME_FLOWERS = "",
-                    LOTS_OF_FLOWERS = "",
+                    GENERIC = "我为她感到遗憾，小姑娘。",
+                    SOME_FLOWERS = "这里还需要更多的花。",
+                    LOTS_OF_FLOWERS = "我们的纪念仪式竟然逆转了引力子的方向...",
                 },
-                GHOSTFLOWER = "",
 
-                GHOSTLYELIXIR_ATTACK = "",
-                GHOSTLYELIXIR_FASTREGEN = "",
-                GHOSTLYELIXIR_RETALIATION = "",
-                GHOSTLYELIXIR_SHIELD = "",
-                GHOSTLYELIXIR_SLOWREGEN = "",
-                GHOSTLYELIXIR_SPEED = "",
+                LOST_TOY_1  = "从理论上讲，这是位面投影的一种。",--plane
+                LOST_TOY_2  = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_7  = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_10 = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_11 = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_14 = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_18 = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_19 = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_42 = "从理论上讲，这是位面投影的一种。",
+                LOST_TOY_43 = "从理论上讲，这是位面投影的一种。",
+        
+                GHOSTFLOWER = "年轻鬼魂催生了花朵形的精神体结构，以此向她表示感谢。",
+
+                GHOSTLYELIXIR_ATTACK = "巧妙地利用了精神体的相互作用，真聪明。",
+                GHOSTLYELIXIR_FASTREGEN = "巧妙地利用了精神体的相互作用，真聪明。",
+                GHOSTLYELIXIR_RETALIATION = "巧妙地利用了精神体的相互作用，真聪明。",
+                GHOSTLYELIXIR_SHIELD = "巧妙地利用了精神体的相互作用，真聪明。",
+                GHOSTLYELIXIR_SLOWREGEN = "巧妙地利用了精神体的相互作用，真聪明。",
+                GHOSTLYELIXIR_SPEED = "巧妙地利用了精神体的相互作用，真聪明。",
 
                 --WX78
                 WX78_SCANNER_ITEM = "是J1-M1，它也一起进来了？",
@@ -554,51 +588,51 @@ local strings = {
 
                 --薇克巴顿
                 BOOKSTATION = {
-                    GENERIC = "",
-                    BURNT = "",
+                    GENERIC = "绝妙啊！无需任何能量物质输入就能自动完成图书的护理修复工作。",--Wonderful!
+                    BURNT = "正如我预料的一样，依然可燃。",
                 },
-                BOOK_BEES = "",
-                BOOK_BIRDS = "",
-                BOOK_BRIMSTONE = "",
-                BOOK_ELEMENTAL = "",--熔炉傀儡书
-                BOOK_FIRE = "",
-                BOOK_FISH = "",
-                BOOK_FOSSIL = "",--熔炉石化书
-                BOOK_GARDENING = "",
-                BOOK_HORTICULTURE = "",
-                BOOK_HORTICULTURE_UPGRADED = "",
-                BOOK_LIGHT = "",
-                BOOK_LIGHT_UPGRADED = "",
-                BOOK_MOON = "",
-                BOOK_RAIN = "",
-                BOOK_RESEARCH_STATION = "",
-                BOOK_SILVICULTURE = "",
-                BOOK_SLEEP = "",
-                BOOK_TEMPERATURE = "",
-                BOOK_TENTACLES = "",
-                BOOK_WEB = "",
+                BOOK_BEES = "我无法翻开它，这些昆虫尾器是如何把书页钉这么死的？",
+                --BOOK_BIRDS = "我无法获取其中的知识。真令人沮丧！",--和单机一样
+                --BOOK_BRIMSTONE = "哦，我真希望我能得到它的力量。",--和单机一样
+                BOOK_ELEMENTAL = "这一本书也被允许用作钝器武器，为什么？",--熔炉傀儡书
+                BOOK_FIRE = "它能在作用范围内控制氧化反应，真迷人！",
+                BOOK_FISH = "控制海洋鱼群，超声波？心灵感应？",
+                BOOK_FOSSIL = "我试过用其它书作为钝器武器，但都被无形的力阻止了，它有什么不同？",--熔炉石化书
+                --BOOK_GARDENING = "不止于收录事实。",--和单机一样
+                BOOK_HORTICULTURE = "您可以再写一本完整版吗？女士？",
+                BOOK_HORTICULTURE_UPGRADED = "看来它的效果与记录的信息量呈正相关。",
+                BOOK_LIGHT = "我尝试读懂这本书但不知为何遭到了阻止。",  --和检查暗影秘典一样 --"My attempts to understand this are being thwarted somehow."
+                BOOK_LIGHT_UPGRADED = "这些光线是从哪里射出的？",
+                BOOK_MOON = "从理论上讲，这能够安全地对“那个东西”施加影响。",
+                BOOK_RAIN = "这将是混沌系统预测的大突破！",
+                BOOK_RESEARCH_STATION = "女士，你是如何让知识扩展自己的？",
+                BOOK_SILVICULTURE = "为什么它无法对一部分植物起效呢？",
+                --BOOK_SLEEP = "这些墨水里面有黑暗燃料吗？真迷人！",--和单机一样
+                BOOK_TEMPERATURE = "对热能的完全控制！真是革命性的技术！",
+                --BOOK_TENTACLES = "是什么在阻止我使用这个？",--和单机一样
+                BOOK_WEB = "有趣。它生成的材料能够识别敌我，选择性地展现粘性。",
 
                 --伍迪
-                LUCY = "奇怪的能量波从它里面散发出来。", --Strange waves of energy are emanating from it.
-                WEREITEM_BEAVER = "",
-                WEREITEM_GOOSE = "",
-                WEREITEM_MOOSE = "",
+                --LUCY = "奇怪的能量波从它里面散发出来。",--和单机一样
+                WEREITEM_BEAVER = "他通过食用这些来定向激活“诅咒”效果。",
+                WEREITEM_GOOSE = "他通过食用这些来定向激活“诅咒”效果。",
+                WEREITEM_MOOSE = "他通过食用这些来定向激活“诅咒”效果。",
 
                 --韦斯来了！！！！！（惊恐）
-                BALLOONS_EMPTY = "",--Balloons? Here?
+                BALLOONS_EMPTY = "看来这是他带进来的。", --单机版："Balloons? Here?"
 
-                BALLOON = "看来这里的大气成分和我们的世界不同。",
+                BALLOON = "也许这里的大气成分和我们的世界不同。",
                 BALLOONPARTY = "",
                 BALLOONSPEED = {
                     GENERIC = "",
                     DEFLATED = "",
                 },
 
-                BALLOONHAT = "",
-                BALLOONVEST = "",
+                BALLOONHAT = "啊，真是个有趣的小玩具。",
+                BALLOONVEST = "提供浮力，简陋但有用。",
 
                 --可怜的Maxy，被她玩弄于股掌之间
-                WAXWELLJOURNAL = "我尝试读懂这本书但不知为何遭到了阻止。" --"My attempts to understand this are being thwarted somehow."
+                --WAXWELLJOURNAL = "我尝试读懂这本书但不知为何遭到了阻止。"--和单机一样
                 TOPHAT_MAGICIAN = "这将会是物流界的伟大革命！",
                 MAGICIAN_CHEST = "绝妙啊！黑暗燃料它的内部生成了一个虫洞。",
 
@@ -705,7 +739,7 @@ local strings = {
                 PORTABLETENT = "",
                 PORTABLETENT_ITEM = "",
 
-                SLINGSHOT = "",
+                SLINGSHOT = "这些血囊展示出了超强的延展性和弹性。",--plasma sacks
                 SLINGSHOTAMMO_ROCK = "",
                 SLINGSHOTAMMO_GOLD = "",
                 SLINGSHOTAMMO_MARBLE = "",
@@ -793,8 +827,85 @@ local strings = {
                     DIFFERENTSHARD = "对裂缝的绝妙应用。",
                 },
                 POCKETWATCH_PORTAL_EXIT = "对裂缝的绝妙应用。",
+
+                --风暴之眼
+                ALTERGUARDIAN_PHASE1 = {
+                    GENERIC = "",
+                    DEAD = "",
+                },
+                ALTERGUARDIAN_PHASE2 = {
+                    GENERIC = "",
+                   DEAD = "",
+                },
+                ALTERGUARDIAN_PHASE2SPIKE = "",
+                ALTERGUARDIAN_PHASE3 = "",
+                ALTERGUARDIAN_PHASE3TRAP = "",
+                ALTERGUARDIAN_PHASE3DEADORB = "",
+                ALTERGUARDIAN_PHASE3DEAD = "",
+
+                ALTERGUARDIANHAT = "",
+                ALTERGUARDIANHATSHARD = "",
+
+                MOONSTORM_GLASS = {
+                    GENERIC = "",v
+                    INFUSED = ""
+                },
+
+                MOONSTORM_STATIC = "",
+                MOONSTORM_STATIC_ITEM = "",
+                MOONSTORM_SPARK = "",
+
+                BIRD_MUTANT = "",
+                BIRD_MUTANT_SPITTER = "",
+
+                WAGSTAFF_NPC = "啊，这是那时我的投影。",
+                ALTERGUARDIAN_CONTAINED = "",
+
+                WAGSTAFF_TOOL_1 = "我的网状缓冲器，以投影形态送到这个位面。",
+                WAGSTAFF_TOOL_2 = "我的装置除垢器，以投影形态送到这个位面。",
+                WAGSTAFF_TOOL_3 = "我的垫圈开槽器，以投影形态送到这个位面。",
+                WAGSTAFF_TOOL_4 = "我的概念刷洗器，以投影形态送到这个位面。",
+                WAGSTAFF_TOOL_5 = "我的校准观察机，以投影形态送到这个位面。",
+
+                MOONSTORM_GOGGLESHAT = "",
+                --[[
+                --单机版各种护目镜的台词，放在这里做参考
+                GOGGLESARMORHAT = "我这么设计以供我在战斗中研究声音的回响。", --"I designed it so I can study sound reverberations while I'm engaged in combat."
+                GOGGLESHEATHAT = "我希望可以用它来观察一些夜行性生物。", --"I do hope to catch a glimpse of the night creature with these."
+                GOGGLESNORMALHAT = "透镜抛光到特定曲度能使我看的更清楚。", --"Refracted lenses polished to a specific curvature so I can see better."
+                GOGGLESSHOOTHAT = "微小的静电能量球能生成不可思议的燃烧弹。", --"Tiny spheres of static energy make a marvelous flaming projectile."
+                ]]
+
+                MOON_DEVICE = {
+                    GENERIC = "",
+                    CONSTRUCTION1 = "",
+                    CONSTRUCTION2 = "",
+                },
             },
-        }]]
+        }
+        --[[
+            -- Indices match ground types in constants.lua
+        NOUNS =
+        {
+            [2] = { "Road", "Path" },
+            [3] = { "Crag", "Area", "Region", "Rocky Place" },
+            [4] = { "Patch", "Turf", "Area", "Tract" },
+            [5] = { "Savannah", "Grassland", "Prairie" },
+            [6] = { "Field", "Pasture", "Meadow", "Garden" },
+            [7] = { "Forest", "Woods", "Thicket", "Grove" },
+            [8] = { "Marsh", "Swamp", "Bog", "Fen" },
+            [9] = { "Web", "Cobweb" },
+            [10] = { "Place", "Corner", "Spot", "Base", "Hidey-Hole" },
+            [11] = { "Place", "Corner", "Spot", "Base", "Hidey-Hole", "Carpet", "Rug" },
+            [12] = { "Place", "Corner", "Spot", "Base", "Checkerboard", "Zone" },
+            [13] = { "Cave" },[14] = { "Cave" },[15] = { "Cave" },[16] = { "Cave" },[17] = { "Cave" },[18] = { "Cave" },
+            [19] = { "Cave" },[20] = { "Cave" },[21] = { "Cave" },[22] = { "Cave" },[23] = { "Cave" },[24] = { "Cave" },
+            [25] = { "Cave" },
+            [30] = { "Forest", "Woods", "Thicket", "Grove" },
+            [31] = { "Desert", "Badlands", "Flats" },
+            [32] = { "Place", "Corner", "Spot", "Base", "Hidey-Hole", "Zone", "Scale" },
+        },]]
+        
     }
 }
 
